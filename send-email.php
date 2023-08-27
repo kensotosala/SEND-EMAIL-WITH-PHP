e96f30dd74b3654b626562f29b29c0a57a6e184e
kendevtech <kendallsalazarsoto@gmail.com>
2023-08-27 17:46:49 -0600

    Create PHP script

---
+++ send-email.php
@@ -0,0 +1,12 @@
+<?php
+// Get the value of the 'name' field from the POST data and store it in the $name variable
+$name = $_POST["name"];
+
+// Get the value of the 'email' field from the POST data and store it in the $email variable
+$email = $_POST["email"];
+
+// Get the value of the 'subject' field from the POST data and store it in the $subject variable
+$subject = $_POST["subject"];
+
+// Get the value of the 'message' field from the POST data and store it in the $message variable
+$message = $_POST["message"];
