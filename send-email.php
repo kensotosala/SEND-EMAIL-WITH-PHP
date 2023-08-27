fb98e829373109eb22dbb574a8195798160fa783
kendevtech <kendallsalazarsoto@gmail.com>
2023-08-27 17:56:47 -0600

    Use PHPMailer

--- send-email.php
+++ send-email.php
@@ -10,3 +10,34 @@ $subject = $_POST["subject"];
 
 // Get the value of the 'message' field from the POST data and store it in the $message variable
 $message = $_POST["message"];
+
+// Include the PHPMailer autoload file
+require "vendor/autoload.php";
+
+// Import necessary classes from the PHPMailer namespace
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+// Create a new instance of the PHPMailer class
+$mail = new PHPMailer(true);
+
+// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+
+// Set up SMTP settings
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host = "smtp.example.com"; // SMTP server host
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Encryption method
+$mail->Port = 587; // SMTP port
+$mail->Password = "password"; // SMTP password
+
+// Set the email subject and body
+$mail->Subject = $subject; // Subject of the email
+$mail->Body = $message; // Body of the email
+
+// Attempt to send the email
+$mail->send();
+
+// Display a success message after sending the email
+echo "email sent";
